c5d7da25df42f157f90be75106d5d0f792c6e265
Jhacker <1281912167@qq.com>
2021-01-31 11:21:17 +0800

    optimized

--- calc/calc.py
+++ calc/calc.py
@@ -1,11 +1,9 @@
-import os
-import sys
-
-import yaml
-
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 class Calc:
-
+    """
+    计算器类
+    """
     def add(self, x, y):
         return x + y
 
